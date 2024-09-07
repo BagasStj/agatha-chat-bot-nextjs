bb5f7f73de8de2362a75a781aeecb861e5832403
Bagas Setiaji <stjbagas28@gmail.com>
2024-09-07 10:12:40 +0700

    update

--- src/app/api/webhook-rs-new/route.ts
+++ src/app/api/webhook-rs-new/route.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 import redisClient from '@/lib/redisClient';
 import { NextRequest, NextResponse } from 'next/server';
-import { prisma } from '@/lib/prisma';
 
+const prisma = new PrismaClient()
 
 export async function POST(req: NextRequest) {
     return handleWebhook(req);
